57002fe086feff54b116b55c5a47b5ecea069907
Maysoon Tabali <mtab03@me.com>
2020-07-20 20:23:03 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -24,7 +24,7 @@ def third_challenge
    :capulet => {
       :patriarch => {
         name: "Lord Capulet", 
-        age: 50
+        age: "50"
         #your key/value pairs here
         },
       :matriarch => {
