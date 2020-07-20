a3ee9d1c41c0bcc6fc98e9daf39b91c1d48c1736
Maysoon Tabali <mtab03@me.com>
2020-07-20 19:59:18 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,7 +1,3 @@
-require_relative 'spec_helper'
-require_relative '../lib/02_second_challenge.rb'
-
-
 #def second_challenge
  # epic_tragedy = {
   #  :montague => {:patriarch, :matriarch, :hero, :hero_friends[ ]};
