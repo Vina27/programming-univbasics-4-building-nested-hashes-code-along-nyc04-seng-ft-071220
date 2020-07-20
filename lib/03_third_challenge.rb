1fcae99be0c9b887676bc8646bf9107f57b254fb
Maysoon Tabali <mtab03@me.com>
2020-07-20 20:13:14 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -9,11 +9,13 @@ def third_challenge
         #your key/value pairs here
         },
       :matriarch => {
-        name: "Lady Montague"
+        name: "Lady Montague", 
         age: "54"
         #your key/value pairs here
         },
       :hero => {
+        name: "Romeo", 
+        age: "15"
         #your key/value pairs here
         },
       :hero_friends => []
